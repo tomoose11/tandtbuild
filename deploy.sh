9fae4e01c97ffdbd4bc9a14a8f132a2300a9ee28
tomoose11 <tomwall_84@hotmail.co.uk>
2021-03-27 18:32:21 +0000

    update

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:tomoose11/tandtbuild.git master:gh-pages
+cd -
