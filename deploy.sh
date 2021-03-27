d81db11bc71c89aa0260f228c6697e9dee618aed
tomoose11 <tomwall_84@hotmail.co.uk>
2021-03-27 20:23:49 +0000

    add

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ set -e
 cd dist
 # if you are deploying to a custom domain    
 # echo 'www.example.com' > CNAME
+git init
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:tomoose11/tandtbuild.git master:gh-pages
